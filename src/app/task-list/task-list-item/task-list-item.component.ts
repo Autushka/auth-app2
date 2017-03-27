995663e9d897d3abb6db566c3a01da94eb62af47
autushka <aleh.autushka@gmail.com>
2017-03-26 21:59:04 -0400

    - event delay added (testing on mobile);

--- src/app/task-list/task-list-item/task-list-item.component.ts
+++ src/app/task-list/task-list-item/task-list-item.component.ts
@@ -1,23 +1,29 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Observable, Subscription} from "rxjs";
 
 @Component({
-   selector: 'app-task-list-item',
-   templateUrl: './task-list-item.component.html',
-   styleUrls: ['./task-list-item.component.css']
+  selector: 'app-task-list-item',
+  templateUrl: './task-list-item.component.html',
+  styleUrls: ['./task-list-item.component.css']
 })
 export class TaskListItemComponent implements OnInit {
-   @Input() taskListItemHeight: number;
-   @Input() taskListItem: any;
-   @Input() taskListItemId: string;
-   @Output() onTaskListItemSelected = new EventEmitter();
+  @Input() taskListItemHeight: number;
+  @Input() taskListItem: any;
+  @Input() taskListItemId: string;
+  @Output() onTaskListItemSelected = new EventEmitter();
+  eventDelay: Observable<number>;
+  eventDelaySubscribe: Subscription;
 
-   constructor() {
-   }
+  constructor() {
+  }
 
-   ngOnInit() {
-   }
+  ngOnInit() {
+  }
 
-   selectTaskListItem() {
+  selectTaskListItem() {
+    this.eventDelay = Observable.timer(100);
+    this.eventDelaySubscribe = this.eventDelay.subscribe(() => {
       this.onTaskListItemSelected.emit(this.taskListItem);
-   }
+    });
+  }
 }
