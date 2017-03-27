d1b8403ebf67e3661311698216beb76f59046897
autushka <aleh.autushka@gmail.com>
2017-03-27 15:46:04 -0400

    event delay adjustments for the navigation

--- src/app/task-list/task-list-item/task-list-item.component.ts
+++ src/app/task-list/task-list-item/task-list-item.component.ts
@@ -21,7 +21,7 @@ export class TaskListItemComponent implements OnInit {
   }
 
   selectTaskListItem() {
-    this.eventDelay = Observable.timer(500);
+    this.eventDelay = Observable.timer(3000);
     this.eventDelaySubscribe = this.eventDelay.subscribe(() => {
       this.onTaskListItemSelected.emit(this.taskListItem);
     });
